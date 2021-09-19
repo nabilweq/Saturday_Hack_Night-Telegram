6b0ba35adca3fff57da6c0781cf262ad70c1f9b1
nabilweq <nabeeltkanr@gmail.com>
2021-09-19 11:23:27 +0530

    Ignored files

--- Team204/bot.js
+++ Team204/bot.js
@@ -6,7 +6,16 @@ const bot = new Telegraf('2022897128:AAEt7h3d5aFPsxOFqngIHdu9s_XTJmBaUqc');
 const apiKey = "23b257f41671a665998ba5b0113df7475c2af77db22d19f491311a2881c937c3"
 
 bot.command('start', ctx => {
-    var startMessage = "Welcome, this bot gives you crypto currency information";
+    sendStartMessage(ctx)
+});
+
+bot.action('start', ctx => {
+    ctx.deleteMessage();
+    sendStartMessage(ctx)
+});
+
+function sendStartMessage(ctx) {
+    let startMessage = "Welcome, this bot gives you crypto currency information";
     bot.telegram.sendMessage(ctx.chat.id, startMessage,
     {
         reply_markup: {
@@ -20,6 +29,29 @@ bot.command('start', ctx => {
             ]
         }
     });
+}
+
+bot.action('price', ctx => {
+    let priceMessage = "Get Price Information. Select one of the cryptocurrencies below";
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, priceMessage,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: "BTC", callback_data: "price-BTC"},
+                        {text: "ETH", callback_data: "price-ETH"},
+                    ],
+                    [
+                        {text: "BCH", callback_data: "price-BCH"},
+                        {text: "LTC", callback_data: "price-LTC"},
+                    ],
+                    [
+                        {text: "Back to Menu", callback_data: "start"}
+                    ]
+                ]
+            }
+        });
 });
 
 bot.launch();
