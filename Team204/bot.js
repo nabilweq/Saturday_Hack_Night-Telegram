f32327d9fb3f84e6542e4c6a3fd8412c4c0c0cbf
nabilweq <nabeeltkanr@gmail.com>
2021-09-19 11:02:45 +0530

    Created the bot and initialized the inline keyboard

--- Team204/bot.js
+++ Team204/bot.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const { Telegraf }  = require('telegraf');
+const axios = require('axios');
+
+const bot = new Telegraf('2022897128:AAEt7h3d5aFPsxOFqngIHdu9s_XTJmBaUqc');
+
+const apiKey = "23b257f41671a665998ba5b0113df7475c2af77db22d19f491311a2881c937c3"
+
+bot.command('start', ctx => {
+    var startMessage = "Welcome, this bot gives you crypto currency information";
+    bot.telegram.sendMessage(ctx.chat.id, startMessage,
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: "Crypto Prices", callback_data: "price"}
+                ],
+                [
+                    {text: "CoinMarketcap", url: "https://www.cryptocompare.com/"}
+                ]
+            ]
+        }
+    });
+});
+
+bot.launch();
